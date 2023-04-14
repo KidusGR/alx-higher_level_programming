039768cc44db25a717e122848193ba351c9211d0
KidusGR <kidmen10@gmail.com>
2023-04-14 18:42:08 +0300

    0x0F. Python - Object-relational mapping

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 """Print State object with the name
    passed as an argument from the 
-   database hbtn_0e_6_usa 
-"""
+   database hbtn_0e_6_usa"""
 
 import sys
 from sqlalchemy import create_engine
