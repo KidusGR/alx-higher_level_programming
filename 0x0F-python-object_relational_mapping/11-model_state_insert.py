8d8c98cb3b154fe09436a00dac7590bd7e508bbf
KidusGR <kidmen10@gmail.com>
2023-04-14 18:30:20 +0300

    0x0F. Python - Object-relational mapping

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-"""
- adds the State object "Louisiana" to the database hbtn_0e_6_usa
-"""
+"""Adds State object "Louisiana" to the database hbtn_0e_6_usa"""
 
 import sys
 from unicodedata import name
@@ -10,9 +8,10 @@ from sqlalchemy.orm import sessionmaker
 from model_state import State
 
 if __name__ == "__main__":
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}".format(sys.argv[1],
+			sys.argv[2],
+			sys.argv[3]),
+			pool_pre_ping=True)
     session_maker = sessionmaker(bind=engine)
     session = session_maker()
 
