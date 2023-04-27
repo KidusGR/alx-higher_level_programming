cbe0b2dd42f7f5dd67809261402031759f7ade37
KidusGR <kidmen10@gmail.com>
2023-04-27 20:10:45 +0300

    0x11. Python - Network #1

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """
-a python script that sends a POST request to http://0.0.0.0:5000/search_user with a given letter.
+a python script that takes in a letter and sends a
+POST request to http://0.0.0.0:5000/search_user
+with the letter as a parameter.
 """
 
 import sys
@@ -10,9 +12,9 @@ if __name__ == "__main__":
     letter = "" if len(sys.argv) == 1 else sys.argv[1]
     payload = {"q": letter}
 
-    r = requests.post("http://0.0.0.0:5000/search_user", data=payload)
+    m = requests.post("http://0.0.0.0:5000/search_user", data=payload)
     try:
-        response = r.json()
+        response = m.json()
         if response == {}:
             print("No result")
         else:
