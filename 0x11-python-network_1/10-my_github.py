340caf833902dc8b8fcdb01f131ee1a203d1dbc8
KidusGR <kidmen10@gmail.com>
2023-04-27 20:14:27 +0300

    0x11. Python - Network #1

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,13 +1,20 @@
 #!/usr/bin/python3
 """
-a python script that Uses the GitHub API to display a GitHub ID based on given credentials.
+a python script that takes GitHub credentials
+(username and password) and uses the GitHub API to display user's id
 """
 
-import sys
-import requests
-from requests.auth import HTTPBasicAuth
-
 if __name__ == "__main__":
-    auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
-    r = requests.get("https://api.github.com/user", auth=auth)
-    print(r.json().get("id"))
+    import requests
+    import sys
+
+    url = "https://api.github.com/user"
+    user = sys.argv[1]
+    passwd = sys.argv[2]
+    data_res = requests.get(url, auth=(user, passwd))
+
+    try:
+        data_json = data_res.json()
+        print(data_json["id"])
+    except Exception:
+        print("None")
