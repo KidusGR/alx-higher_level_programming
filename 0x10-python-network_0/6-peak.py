de9e6f091a271e36dd80ed2f9b922168ce34f419
KidusGR <kidmen10@gmail.com>
2023-04-27 13:59:25 +0300

    0x10. Python - Network #0

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-a function that finds a peak in a list of unsorted integers.
+a Python function that finds a peak in a list of unsorted integers.
 """
 
 
@@ -8,6 +8,7 @@ def find_peak(list_of_integers):
     """
     Return a peak in a list of unsorted integers
     """
+
     if len(list_of_integers) == 0:
         return None
     if len(list_of_integers) == 1:
